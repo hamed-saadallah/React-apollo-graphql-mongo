178a41d4afead2c19e54fa4c693278a30e556d77
Sawsen <sawsen.fattahi@moez.fr>
2018-03-23 12:08:43 +0100

    clean

--- firstServer/src/graphql/Blocks/blockQueries.js
+++
@@ -1,9 +0,0 @@
-import { BlocksType } from './blockTypes';
-import getBlocks from '../../services/blocksServices/getBlocks';
-
-const blocksQuery = {
-  type: BlocksType,
-  resolve: () => getBlocks()
-}
-console.log('blocksQuery', BlocksType);
-export { blocksQuery }
